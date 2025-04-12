af10ff037b21ab3642d048e1943f81f5725253a3
DarthAkiraNihil <egorzverev666@gmail.com>
2025-04-12 21:35:01 +0300

    [#4] feat: Add KJsonToken enum for json parsing

---
+++ src/main/java/io/github/darthakiranihil/konna/core/data/json/KJsonToken.java
@@ -0,0 +1,57 @@
+package io.github.darthakiranihil.konna.core.data.json;
+
+/**
+ * Enumeration that describes a json token
+ */
+public enum KJsonToken {
+
+    /**
+     * Symbol of "{"
+     */
+    OPEN_BRACE,
+    /**
+     * Symbol of "}"
+     */
+    CLOSE_BRACE,
+
+    /**
+     * Symbol of "["
+     */
+    OPEN_SQUARE_BRACKET,
+    /**
+     * Symbol of "]"
+     */
+    CLOSE_SQUARE_BRACKET,
+
+    /**
+     * A string
+     */
+    STRING,
+    /**
+     * An integer or a float (can be written in exponential form)
+     */
+    NUMBER,
+
+    /**
+     * Symbol of ","
+     */
+    COMMA,
+    /**
+     * Symbol of ":"
+     */
+    SEMICOLON,
+
+    /**
+     * True literal
+     */
+    TRUE,
+    /**
+     * False literal
+     */
+    FALSE,
+    /**
+     * Null literal
+     */
+    NULL
+
+}
