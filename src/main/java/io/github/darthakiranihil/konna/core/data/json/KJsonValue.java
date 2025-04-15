17c528ca243f2376de1118a2f2a6d87dd519e996
DarthAkiraNihil <egorzverev666@gmail.com>
2025-04-15 21:31:34 +0300

    [#4] feat: Implement KJsonValue, sorta

--- src/main/java/io/github/darthakiranihil/konna/core/data/json/KJsonValue.java
+++ src/main/java/io/github/darthakiranihil/konna/core/data/json/KJsonValue.java
@@ -3,10 +3,9 @@ package io.github.darthakiranihil.konna.core.data.json;
 import io.github.darthakiranihil.konna.core.data.json.except.KJsonValueException;
 
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
+import java.util.Map;
 
-public class KJsonValue implements Iterable<KJsonValue> {
+public class KJsonValue {
 
     private final KJsonValueType type;
     private final Object value;
@@ -20,7 +19,11 @@ public class KJsonValue implements Iterable<KJsonValue> {
         return type;
     }
 
-    @Override
+    public boolean isNull() {
+        return this.value == null || this.type == KJsonValueType.NULL;
+    }
+
+    @SuppressWarnings("unchecked")
     public Iterator<KJsonValue> iterator() {
         if (this.type != KJsonValueType.ARRAY) {
             throw new KJsonValueException(
@@ -31,13 +34,95 @@ public class KJsonValue implements Iterable<KJsonValue> {
         return ((Iterable<KJsonValue>) this.value).iterator();
     }
 
-    @Override
-    public void forEach(Consumer<? super KJsonValue> action) {
-        Iterable.super.forEach(action);
+    @SuppressWarnings("unchecked")
+    public KJsonValue getProperty(String key) {
+        if (this.type != KJsonValueType.OBJECT) {
+            throw new KJsonValueException(
+                String.format("Cannot get property from the json value: it's not an object. The actual type is: %s", this.type)
+            );
+        }
+
+        return ((Map<String, KJsonValue>) this.value).get(key);
     }
 
-    @Override
-    public Spliterator<KJsonValue> spliterator() {
-        return Iterable.super.spliterator();
+    public boolean getBoolean() {
+        if (this.type != KJsonValueType.BOOLEAN) {
+            throw new KJsonValueException(
+                String.format("Cannot get boolean from the json value: it's not a boolean. The actual type is: %s", this.type)
+            );
+        }
+
+        return (boolean) this.value;
     }
+
+    public int getInt() {
+        if (this.type != KJsonValueType.NUMBER) {
+            throw new KJsonValueException(
+                String.format("Cannot get int from the json value: it's not an int. The actual type is: %s", this.type)
+            );
+        }
+
+        return (int) this.value;
+    }
+
+    public long getLong() {
+        if (this.type != KJsonValueType.NUMBER) {
+            throw new KJsonValueException(
+                String.format("Cannot get long from the json value: it's not a long. The actual type is: %s", this.type)
+            );
+        }
+
+        return (long) this.value;
+    }
+
+    public float getFloat() {
+        if (this.type != KJsonValueType.NUMBER) {
+            throw new KJsonValueException(
+                String.format("Cannot get float from the json value: it's not a float. The actual type is: %s", this.type)
+            );
+        }
+
+        return (float) this.value;
+    }
+
+    public double getDouble() {
+        if (this.type != KJsonValueType.NUMBER) {
+            throw new KJsonValueException(
+                String.format("Cannot get double from the json value: it's not a double. The actual type is: %s", this.type)
+            );
+        }
+
+        return (double) this.value;
+    }
+
+    public String getString() {
+        if (this.type != KJsonValueType.STRING) {
+            throw new KJsonValueException(
+                String.format("Cannot get string from the json value: it's not a string. The actual type is: %s", this.type)
+            );
+        }
+
+        if (this.isNull()) {
+            return null;
+        }
+
+        return (String) this.value;
+    }
+
+    public KJsonValue getObject() {
+        if (this.type != KJsonValueType.OBJECT) {
+            throw new KJsonValueException(
+                String.format("Cannot get object from the json value: it's not an int. The actual type is: %s", this.type)
+            );
+        }
+
+        if (this.isNull()) {
+            return null;
+        }
+
+        return (KJsonValue) this.value;
+    }
+
+
+
 }
