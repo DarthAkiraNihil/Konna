e77c1bc95cdbf2f069774920fd55524c56523119
DarthAkiraNihil <egorzverev666@gmail.com>
2025-08-02 14:02:51 +0300

    [#5] feat: add base for de/serializer implementation

---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/data/json/std/KStandardJsonDeserializer.java
@@ -0,0 +1,12 @@
+package io.github.darthakiranihil.konna.core.data.json.std;
+
+import io.github.darthakiranihil.konna.core.data.json.KJsonDeserializer;
+import io.github.darthakiranihil.konna.core.data.json.KJsonValue;
+
+public class KStandardJsonDeserializer implements KJsonDeserializer {
+
+    @Override
+    public <T> T deserialize(KJsonValue value, Class<T> clazz) {
+        return null;
+    }
+}
---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/data/json/std/KStandardJsonSerializer.java
@@ -0,0 +1,12 @@
+package io.github.darthakiranihil.konna.core.data.json.std;
+
+import io.github.darthakiranihil.konna.core.data.json.KJsonSerializer;
+import io.github.darthakiranihil.konna.core.data.json.KJsonValue;
+
+public class KStandardJsonSerializer implements KJsonSerializer {
+
+    @Override
+    public <T> KJsonValue serialize(Object object, Class<T> clazz) {
+        return null;
+    }
+}
