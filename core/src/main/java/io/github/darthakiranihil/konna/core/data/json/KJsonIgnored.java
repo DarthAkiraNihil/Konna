a11a2d8c4766bd5233430a6ac87eae9b3ca2aa08
DarthAkiraNihil <egorzverev666@gmail.com>
2025-08-02 17:38:43 +0300

    [#6] doc: add docs for annotations

--- core/src/main/java/io/github/darthakiranihil/konna/core/data/json/KJsonIgnored.java
+++ core/src/main/java/io/github/darthakiranihil/konna/core/data/json/KJsonIgnored.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the field should be ignored in serialization process
+ * and not regenerated during deserialization
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface KJsonIgnored {
