5c88164f94f0252e14458f9110ccb6865c3757ce
DarthAkiraNihil <egorzverev666@gmail.com>
2025-09-13 10:28:36 +0300

    [#22] feat: add KContainer

--- core/src/main/java/io/github/darthakiranihil/konna/core/di/KContainer.java
+++ core/src/main/java/io/github/darthakiranihil/konna/core/di/KContainer.java
@@ -16,5 +16,80 @@
 
 package io.github.darthakiranihil.konna.core.di;
 
-public class KContainer {
+import io.github.darthakiranihil.konna.core.di.except.KDependencyResolveException;
+import io.github.darthakiranihil.konna.core.object.KObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple container for interface-implementation class mapping, that is
+ * useful when resolving and injecting dependencies. Supports hierarchical
+ * containers that means a parent container can be specified for it.
+ *
+ * @since 0.2.0
+ * @author Darth Akira Nihil
+ */
+public final class KContainer extends KObject {
+
+    private final Map<Class<?>, Class<?>> classMap;
+    private final KContainer parent;
+
+    /**
+     * Zero-arg constructor. Constructs a container without parent one.
+     */
+    public KContainer() {
+        this.classMap = new HashMap<>();
+        this.parent = null;
+    }
+
+    /**
+     * Constructs container with specified parent container.
+     * @param parent Parent container
+     */
+    public KContainer(final KContainer parent) {
+        this.classMap = new HashMap<>();
+        this.parent = parent;
+    }
+
+    /**
+     * Adds a binding from a given interface to specified implementation of if.
+     * @param fromInterface Interface to be bind
+     * @param toImplementation Implementation to the interface
+     * @return This container
+     * @param <IN> Type of the interface
+     */
+    public <IN> KContainer add(
+        final Class<IN> fromInterface,
+        final Class<? extends IN> toImplementation
+    ) {
+        this.classMap.put(fromInterface, toImplementation);
+        return this;
+    }
+
+
+    /**
+     * Resolves an implementation (or just maps one to another) for given class.
+     * The common example is getting class of the implementation of an interface.
+     * When resolving, container's classmap has higher priority than its parent container,
+     * so if one interface has different mappings in this and parent container, this
+     * container's mapping will be returned first. If it fails to find a mapping,
+     * {@link KDependencyResolveException} will be thrown.
+     * @param clazz Class to resolve
+     * @return Mapped resolved class
+     * @param <T> Type of resolved class
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Class<T> resolve(final Class<T> clazz) {
+        if (this.classMap.containsKey(clazz)) {
+            return (Class<T>) this.classMap.get(clazz);
+        }
+
+        if (this.parent != null) {
+            return this.parent.resolve(clazz);
+        }
+
+        throw new KDependencyResolveException(clazz);
+    }
+
 }
---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/di/except/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides exceptions connected with different dependency injection errors.
+ *
+ * @since 0.2.0
+ * @author Darth Akira Nihil
+ */
+package io.github.darthakiranihil.konna.core.di.except;
