0383bbb9501fb754d88d0e678cf466866917c087
DarthAkiraNihil <egorzverev666@gmail.com>
2025-10-19 17:27:52 +0300

    [#18] feat: make container resolver an interface

--- core/src/main/java/io/github/darthakiranihil/konna/core/engine/KEngineContextLoader.java
+++ core/src/main/java/io/github/darthakiranihil/konna/core/engine/KEngineContextLoader.java
@@ -1,5 +1,17 @@
 package io.github.darthakiranihil.konna.core.engine;
 
+/**
+ * Represents interface for loading the engine context, which
+ * means it must initialize all essential classes.
+ *
+ * @since 0.2.0
+ * @author Darth Akira Nihil
+ */
 public interface KEngineContextLoader {
+    /**
+     * Loads the engine context - initializes all required classes
+     * and returns it.
+     * @return Loaded context
+     */
     KEngineContext load();
 }
