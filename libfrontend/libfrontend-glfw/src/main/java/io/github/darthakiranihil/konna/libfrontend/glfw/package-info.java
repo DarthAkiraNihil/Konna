c58cbf23548bab4f44fef56c1b8b465be2dca968
DarthAkiraNihil <egorzverev666@gmail.com>
2025-12-25 16:36:52 +0300

    [#55] feat: add KGlfw interface and GLFW types abstractions

---
+++ libfrontend/libfrontend-glfw/src/main/java/io/github/darthakiranihil/konna/libfrontend/glfw/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Provides the library frontend for GLFW and some implementations
+ * of classes that uses this library frontend.
+ *
+ * @author Darth Akira Nihil
+ * @since 0.1.0
+ */
+@KIndexedPackage
+@KPackageEnvironment(name = "libfrontend")
+package io.github.darthakiranihil.konna.libfrontend.glfw;
+
+import io.github.darthakiranihil.konna.core.di.KPackageEnvironment;
+import io.github.darthakiranihil.konna.core.util.KIndexedPackage;
