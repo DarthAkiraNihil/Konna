63b6bd6bf8461f62fcd68a82331717c7afb45e6a
DarthAkiraNihil <egorzverev666@gmail.com>
2025-12-26 10:35:55 +0300

    [#61] test: begin making tests

---
+++ components/graphics/src/test/java/io/github/darthakiranihli/konna/graphics/KGraphicsComponentPositiveTests.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihli.konna.graphics;
+
+import io.github.darthakiranihil.konna.core.app.std.KStandardApplicationFeatures;
+import io.github.darthakiranihil.konna.core.data.json.KJsonValue;
+import io.github.darthakiranihil.konna.core.data.json.except.KJsonParseException;
+import io.github.darthakiranihil.konna.core.data.json.except.KJsonSerializationException;
+import io.github.darthakiranihil.konna.core.engine.KComponent;
+import io.github.darthakiranihil.konna.core.engine.KEngineHypervisor;
+import io.github.darthakiranihil.konna.core.engine.KEngineHypervisorConfig;
+import io.github.darthakiranihil.konna.core.test.KStandardTestClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KGraphicsComponentPositiveTests extends KStandardTestClass {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testLoadKonnaWithGraphicsComponentSuccess() {
+
+        System.out.println(Arrays.toString(Package.getPackages()));
+
+        String config = """
+            {
+                "context_loader": "io.github.darthakiranihil.konna.core.engine.impl.TestContextLoader",
+                "component_loader": "io.github.darthakiranihil.konna.core.engine.std.KStandardComponentLoader",
+                "service_loader": "io.github.darthakiranihil.konna.core.engine.std.KStandardServiceLoader",
+                "route_configurers": [
+                    "io.github.darthakiranihil.konna.core.engine.impl.EmptyRouteConfigurer"
+                ],
+                "event_registerers": [
+                    "io.github.darthakiranihil.konna.core.engine.impl.EmptyEventRegisterer"
+                ],
+                "components": [
+                    "io.github.darthakiranihil.konna.graphics.KGraphicsComponent"
+                ]
+            }""";
+
+        KJsonValue parsed;
+        try {
+            parsed = this.jsonParser.parse(config);
+        } catch (KJsonParseException e) {
+            Assertions.fail(e);
+            return;
+        }
+
+        KEngineHypervisorConfig loadedConfig;
+        try {
+            loadedConfig = this.jsonDeserializer.deserialize(
+                parsed,
+                KEngineHypervisorConfig.class
+            );
+        } catch (KJsonSerializationException e) {
+            Assertions.fail(e);
+            return;
+        }
+
+        Assertions.assertNotNull(loadedConfig);
+        KEngineHypervisor hypervisor = new KEngineHypervisor(loadedConfig);
+        hypervisor.launch(new KStandardApplicationFeatures(new HashMap<>()));
+        try {
+            Field engineComponents = KEngineHypervisor.class.getDeclaredField("engineComponents");
+
+            engineComponents.setAccessible(true);
+
+            Map<String, KComponent> mapOfComponents = (Map<String, KComponent>) engineComponents.get(hypervisor);
+            Assertions.assertTrue(mapOfComponents.containsKey("Graphics"));
+
+        } catch (Throwable e) {
+            Assertions.fail(e);
+        }
+
+    }
+}
