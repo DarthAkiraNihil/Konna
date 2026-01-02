8838086a056304615a9f9f37c619ea8f648df146
DarthAkiraNihil <egorzverev666@gmail.com>
2026-01-02 13:08:04 +0300

    [#63] feat: add converting message to KRenderable

--- components/graphics/src/main/java/io/github/darthakiranihil/konna/graphics/service/KInternals.java
+++ components/graphics/src/main/java/io/github/darthakiranihil/konna/graphics/service/KInternals.java
@@ -16,10 +16,15 @@
 
 package io.github.darthakiranihil.konna.graphics.service;
 
+import io.github.darthakiranihil.konna.core.data.json.KJsonDeserializer;
+import io.github.darthakiranihil.konna.core.data.json.KJsonValue;
 import io.github.darthakiranihil.konna.core.di.KInject;
 import io.github.darthakiranihil.konna.core.engine.KMessageToEndpointConverter;
+import io.github.darthakiranihil.konna.core.log.KSystemLogger;
 import io.github.darthakiranihil.konna.core.message.KMessage;
+import io.github.darthakiranihil.konna.core.message.except.KInvalidMessageException;
 import io.github.darthakiranihil.konna.core.object.KUninstantiable;
+import io.github.darthakiranihil.konna.graphics.except.KInvalidRenderableClassException;
 import io.github.darthakiranihil.konna.graphics.internal.KRenderableObjectTypeMapping;
 import io.github.darthakiranihil.konna.graphics.render.KRenderable;
 import org.jetbrains.annotations.ApiStatus;
@@ -30,14 +35,53 @@ final class KInternals extends KUninstantiable {
     public static final class MessageToRenderableConverter implements KMessageToEndpointConverter {
 
         private final KRenderableObjectTypeMapping mapping;
+        private final KJsonDeserializer deserializer;
 
-        public MessageToRenderableConverter(@KInject final KRenderableObjectTypeMapping mapping) {
+        public MessageToRenderableConverter(
+            @KInject final KRenderableObjectTypeMapping mapping,
+            @KInject final KJsonDeserializer deserializer
+        ) {
             this.mapping = mapping;
+            this.deserializer = deserializer;
         }
 
         @Override
         public Object[] convert(final KMessage message) {
-            return new Object[] {new KRenderable.EMPTY()};
+
+            var body = message.body();
+
+            if (!body.containsKey("type")) {
+                throw new KInvalidMessageException("Could not get renderable object type in the message");
+            }
+
+            String type = body.get("type", String.class);
+            Class<? extends KRenderable> renderableClass = this.mapping.getMapping(type);
+            if (renderableClass == null) {
+                throw new KInvalidRenderableClassException(
+                    String.format(
+                        "Unknown renderable type: %s",
+                        type
+                    )
+                );
+            }
+
+            if (!body.containsKey("data")) {
+                throw new KInvalidMessageException("Could not get renderable object data in the message");
+            }
+            KJsonValue data = body.get("data", KJsonValue.class);
+
+            // todo: validation
+            KRenderable renderable = this.deserializer.deserialize(data, renderableClass);
+            if (renderable == null) {
+                KSystemLogger.warning(
+                    "Graphics",
+                    "Could not create renderable object from the message: %s",
+                    type
+                );
+                return new Object[] { new KRenderable.EMPTY() };
+            }
+
+            return new Object[] { renderable };
         }
     }
 
---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/message/except/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides exceptions connected with messages and events.
+ *
+ * @since 0.1.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+package io.github.darthakiranihil.konna.core.message.except;
+
+import org.jspecify.annotations.NullMarked;
