2291f18db87eb053e85b2f4eaf367e32e1c0e7f0
DarthAkiraNihil <egorzverev666@gmail.com>
2026-01-02 08:04:55 +0300

    [#63] feat: add application's Frame

---
+++ rendering/glfw/src/main/java/io/github/darthakiranihil/konna/graphics/glfw/KGlfwFrame.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.graphics.glfw;
+
+import io.github.darthakiranihil.konna.core.di.KInject;
+import io.github.darthakiranihil.konna.core.object.KObject;
+import io.github.darthakiranihil.konna.core.struct.KSize;
+import io.github.darthakiranihil.konna.graphics.KFrame;
+import io.github.darthakiranihil.konna.graphics.except.KInvalidGraphicsStateException;
+import io.github.darthakiranihil.konna.libfrontend.glfw.KGlfw;
+import io.github.darthakiranihil.konna.libfrontend.glfw.KGlfwCallbacks;
+
+import java.nio.IntBuffer;
+
+public class KGlfwFrame extends KObject implements KFrame {
+
+    private final KGlfw glfw;
+    private final KGlfwCallbacks glfwCallbacks;
+    private final IntBuffer pWidth;
+    private final IntBuffer pHeight;
+
+    private long handle;
+    private KSize currentSize;
+
+
+    public KGlfwFrame(
+        @KInject final KGlfw glfw,
+        @KInject final KGlfwCallbacks glfwCallbacks,
+        final String title,
+        final KSize size
+    ) {
+        this.glfw = glfw;
+        this.glfwCallbacks = glfwCallbacks;
+
+        if (!this.glfw.glfwInit()) {
+            throw new KInvalidGraphicsStateException(
+                "Unable to initialize frame: GLFW initialization failed"
+            );
+        }
+
+        this.setResizable(true);
+
+        this.handle = this.glfw.glfwCreateWindow(
+            size.width(),
+            size.height(),
+            title,
+            0,
+            0
+        );
+
+        if (this.handle == 0) {
+            throw new KInvalidGraphicsStateException("Unable to initialize frame: failed to create GLFW window");
+        }
+
+        glfw.glfwMakeContextCurrent(this.handle);
+        glfw.glfwSwapInterval(1);
+
+        this.pHeight = IntBuffer.allocate(1);
+        this.pWidth = IntBuffer.allocate(1);
+        this.updateSize();
+
+    }
+
+    @Override
+    public void setTitle(final String title) {
+        this.glfw.glfwSetWindowTitle(this.handle, title);
+    }
+
+    @Override
+    public KSize getSize() {
+        return this.currentSize;
+    }
+
+    @Override
+    public void setSize(KSize newSize) {
+        this.glfw.glfwSetWindowSize(this.handle, newSize.width(), newSize.height());
+        this.currentSize = newSize;
+    }
+
+    @Override
+    public void setResizable(boolean state) {
+
+    }
+
+    @Override
+    public void terminate() {
+        this.glfwCallbacks.glfwFreeCallbacks(this.handle);
+
+        this.glfw.glfwDestroyWindow(this.handle);
+        this.glfw.glfwTerminate();
+        this.glfwCallbacks.freeLastCallback(this.handle);
+    }
+
+    @Override
+    public void show() {
+        this.glfw.glfwShowWindow(this.handle);
+    }
+
+    @Override
+    public void hide() {
+        this.glfw.glfwHideWindow(this.handle);
+    }
+
+    private void updateSize() {
+        this.glfw.glfwGetWindowSize(this.handle, this.pWidth, this.pHeight);
+        this.currentSize = new KSize(pWidth.get(0), pHeight.get(0));
+
+    }
+
+    @Override
+    public boolean shouldClose() {
+        return this.glfw.glfwWindowShouldClose(this.handle);
+    }
+
+    @Override
+    public void setShouldClose(boolean flag) {
+        this.glfw.glfwSetWindowShouldClose(this.handle, flag);
+    }
+
+    @Override
+    public void swapBuffers() {
+        this.glfw.glfwSwapBuffers(this.handle);
+    }
+}
---
+++ rendering/glfw/src/main/java/io/github/darthakiranihil/konna/graphics/glfw/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Provides rendering classes implementations using
+ * {@link io.github.darthakiranihil.konna.libfrontend.glfw.KGlfw}.
+ *
+ * @since 0.1.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+@KIndexedPackage
+package io.github.darthakiranihil.konna.graphics.glfw;
+
+import io.github.darthakiranihil.konna.core.util.KIndexedPackage;
+import org.jspecify.annotations.NullMarked;
