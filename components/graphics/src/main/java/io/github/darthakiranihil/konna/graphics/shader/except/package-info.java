a3a252cb74c50a4397b86b406a3198240f2477f7
DarthAkiraNihil <egorzverev666@gmail.com>
2026-01-06 09:21:17 +0300

    [#64] feat: add abstractions for shaders

---
+++ components/graphics/src/main/java/io/github/darthakiranihil/konna/graphics/shader/except/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides exceptions connected with different shader errors.
+ *
+ * @since 0.1.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+package io.github.darthakiranihil.konna.graphics.shader.except;
+
+import org.jspecify.annotations.NullMarked;
