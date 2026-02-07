2903548bf1cd3b657b5eb3c0f045ea73f5fa213a
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-07 17:11:10 +0300

    [#115] feat: add asset type definition interface

---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/io/KAssetTypeDefinition.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.core.io;
+
+import io.github.darthakiranihil.konna.core.util.KValidator;
+
+/**
+ * Interface that in encapsulates asset type definition in simple class
+ * that provides validation mechanism to define whether an asset definition
+ * is valid for this type or not.
+ *
+ * @since 0.4.0
+ * @author Darth Akira Nihil
+ */
+public interface KAssetTypeDefinition extends KValidator<KAssetDefinition> {
+
+    /**
+     * @return Name of the type
+     */
+    String getName();
+
+}
