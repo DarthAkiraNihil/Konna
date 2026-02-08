9561f29e5de1946e91239a470220f83592410aa4
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-08 19:24:34 +0300

    [#115] feat: base for a new asset loader

---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/io/std/KJsonTransformerBasedAssetLoader.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.core.io.std;
+
+import io.github.darthakiranihil.konna.core.io.KAsset;
+import io.github.darthakiranihil.konna.core.io.KAssetDefinition;
+import io.github.darthakiranihil.konna.core.io.KAssetLoader;
+import io.github.darthakiranihil.konna.core.io.KAssetTypedef;
+
+public class KJsonTransformerBasedAssetLoader implements KAssetLoader {
+
+    @FunctionalInterface
+    public interface AssetTransformer {
+
+        KAssetDefinition transform(KAssetDefinition value);
+
+    }
+
+    @Override
+    public KAsset loadAsset(String assetId, String typeAlias) {
+        return null;
+    }
+
+    @Override
+    public void addAssetTypedef(KAssetTypedef typedef) {
+
+    }
+
+    @Override
+    public void addNewAsset(String assetId, String internalType, KAssetDefinition rawDefinition) {
+
+    }
+}
