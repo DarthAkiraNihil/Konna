7803bfa6d350b4e8d4b0d0c39565e8e1cac57d67
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-17 19:44:06 +0300

    [#117] feat: add entity component class and a little service

---
+++ components/component-entity/src/main/java/io/github/darthakiranihil/konna/entity/package-info.java
@@ -0,0 +1,6 @@
+@NullMarked
+@KIndexedPackage
+package io.github.darthakiranihil.konna.entity;
+
+import io.github.darthakiranihil.konna.core.util.KIndexedPackage;
+import org.jspecify.annotations.NullMarked;
