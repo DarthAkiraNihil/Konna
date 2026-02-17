45cd657d4ef0860eaa48903d98391aebf4ed3513
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-17 20:04:44 +0300

    [#117] style: fix codestyle

---
+++ components/component-entity/src/main/java/io/github/darthakiranihil/konna/entity/service/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides services used by Konna Entity component.
+ *
+ * @since 0.4.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+package io.github.darthakiranihil.konna.entity.service;
+
+import org.jspecify.annotations.NullMarked;
