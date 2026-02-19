131262090250599be40665d572b06c97e1170132
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-19 08:10:08 +0300

    [#129] feat: add standard entity factory

---
+++ components/component-entity/src/main/java/io/github/darthakiranihil/konna/entity/std/KStandardEntityFactory.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.entity.std;
+
+import io.github.darthakiranihil.konna.core.data.json.KJsonDeserializer;
+import io.github.darthakiranihil.konna.core.data.json.KJsonValue;
+import io.github.darthakiranihil.konna.core.di.KInject;
+import io.github.darthakiranihil.konna.core.object.KActivator;
+import io.github.darthakiranihil.konna.core.object.KObject;
+import io.github.darthakiranihil.konna.core.object.KSingleton;
+import io.github.darthakiranihil.konna.core.object.KTag;
+import io.github.darthakiranihil.konna.core.struct.KStructUtils;
+import io.github.darthakiranihil.konna.entity.KEntity;
+import io.github.darthakiranihil.konna.entity.KEntityDataComponent;
+import io.github.darthakiranihil.konna.entity.KEntityFactory;
+import io.github.darthakiranihil.konna.entity.KEntityMetadata;
+import io.github.darthakiranihil.konna.entity.asset.KEntityMetadataCollection;
+import io.github.darthakiranihil.konna.entity.except.KEntityException;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+@KSingleton
+public class KStandardEntityFactory extends KObject implements KEntityFactory {
+
+    private final KEntityMetadataCollection metadataCollection;
+    private final KActivator activator;
+    private final KJsonDeserializer deserializer;
+
+    public KStandardEntityFactory(
+        @KInject final KEntityMetadataCollection metadataCollection,
+        @KInject final KActivator activator,
+        @KInject final KJsonDeserializer deserializer
+    ) {
+        super("std_entity_factory", KStructUtils.setOfTags(KTag.DefaultTags.STD));
+
+        this.metadataCollection = metadataCollection;
+        this.activator = activator;
+        this.deserializer = deserializer;
+    }
+
+    @Override
+    public KEntity createEntity(String name, String type) {
+        KEntityMetadata metadata = this.metadataCollection.getAsset(type);
+
+        Set<Class<? extends KEntityDataComponent>> dataComponents = new HashSet<>(List.of(metadata.dataComponents()));
+        for (String extension: metadata.dataExtensionList()) {
+            KEntityMetadata superType = this.metadataCollection.getAsset(extension);
+            dataComponents.addAll(List.of(superType.dataComponents()));
+        }
+
+        List<KEntityDataComponent> createdComponents = new LinkedList<>();
+        for (var dataComponent: dataComponents) {
+            createdComponents.add(
+                this.activator.createObject(dataComponent)
+            );
+        }
+
+        return new KEntity(name, createdComponents);
+    }
+
+    @Override
+    public KEntity createEntity(String name, String type, KJsonValue data) {
+        KEntityMetadata metadata = this.metadataCollection.getAsset(type);
+
+        Set<Class<? extends KEntityDataComponent>> dataComponents = new HashSet<>(List.of(metadata.dataComponents()));
+        for (String extension: metadata.dataExtensionList()) {
+            KEntityMetadata superType = this.metadataCollection.getAsset(extension);
+            dataComponents.addAll(List.of(superType.dataComponents()));
+        }
+
+        List<KEntityDataComponent> createdComponents = new LinkedList<>();
+        for (var dataComponent: dataComponents) {
+            KEntityDataComponent deserialized = this.deserializer.deserialize(
+                data.getProperty(dataComponent.getCanonicalName()),
+                dataComponent
+            );
+
+            if (deserialized == null) {
+                throw new KEntityException(
+                    String.format(
+                        "Could not create entity %s: deserialization for data component %s failed",
+                        type,
+                        dataComponent.getCanonicalName()
+                    )
+                );
+            }
+
+            createdComponents.add(
+                deserialized
+            );
+        }
+
+        return new KEntity(name, createdComponents);
+    }
+}
---
+++ components/component-entity/src/main/java/io/github/darthakiranihil/konna/entity/std/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.darthakiranihil.konna.entity.std;
+
+import org.jspecify.annotations.NullMarked;
