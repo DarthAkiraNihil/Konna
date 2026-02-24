c654f3bab88d6226e3fbc49d43d0724708545919
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-24 14:55:49 +0300

    [#149] feat: add KBodyValue annotation and annotation processor for KServiceEndpoint

---
+++ compilers/compiler-core/src/main/java/io/github/darthakiranihil/konna/compiler/core/KServiceEndpointAnnotationProcessor.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.compiler.core;
+
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes({
+    "io.github.darthakiranihil.konna.core.engine.Kse2"
+})
+@SupportedSourceVersion(SourceVersion.RELEASE_21)
+public final class KServiceEndpointAnnotationProcessor extends AbstractProcessor {
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
