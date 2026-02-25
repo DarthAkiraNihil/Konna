db9e4b92e62ba12e18cca1afe084eed301b539d6
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-25 07:45:39 +0300

    [#150] feat: add level component class

---
+++ components/component-level/src/main/java/io/github/darthakiranihil/konna/level/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Level engine component of Konna.
+ *
+ * @since 0.5.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+@KIndexedPackage
+package io.github.darthakiranihil.konna.level;
+
+import io.github.darthakiranihil.konna.core.util.KIndexedPackage;
+import org.jspecify.annotations.NullMarked;
