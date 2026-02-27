d2f0913d0950a60d99841d0b96fb911f66544b6d
DarthAkiraNihil <egorzverev666@gmail.com>
2026-02-27 20:33:31 +0300

    [#154] docs: add docs

--- components/component-level/src/main/java/io/github/darthakiranihil/konna/level/type/package-info.java
+++ components/component-level/src/main/java/io/github/darthakiranihil/konna/level/type/package-info.java
@@ -1,3 +1,9 @@
+/**
+ * Provides classes, representing asset type definitions for Level component.
+ *
+ * @since 0.4.0
+ * @author Darth Akira Nihil
+ */
 @NullMarked
 package io.github.darthakiranihil.konna.level.type;
 
