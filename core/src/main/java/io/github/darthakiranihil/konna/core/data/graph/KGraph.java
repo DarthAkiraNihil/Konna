032e61ea118ee398f5c76a14c748c4ad207d72c2
DarthAkiraNihil <egorzverev666@gmail.com>
2026-03-06 14:13:09 +0300

    [#174] feat: add graph and its default implementation

---
+++ core/src/main/java/io/github/darthakiranihil/konna/core/data/graph/KGraph.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.core.data.graph;
+
+import io.github.darthakiranihil.konna.core.struct.KPair;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+import java.util.Set;
+
+public interface KGraph<IDX, COL, W> extends Iterable<KGraph.Node<IDX, COL, W>> {
+
+    interface Node<IDX, COL, W> {
+        IDX index();
+        COL color();
+        Set<KPair<W, Node<IDX, COL, W>>> adjacent();
+    }
+
+    Set<IDX> getNodeIndices();
+    void clear();
+
+    void add(IDX index, COL color);
+    @Nullable Node<IDX, COL, W> get(IDX index);
+
+    void connect(IDX src, IDX dst, W weight);
+    void biConnect(IDX src, IDX dst, W weight);
+
+    List<KPair<IDX, W>> getPath(IDX src, IDX dst);
+
+}
