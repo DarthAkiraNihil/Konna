6cdff3f670595f9a4dabd86b4dfb668097edd5c0
DarthAkiraNihil <egorzverev666@gmail.com>
2026-03-10 18:05:44 +0300

    [#175] test: add tests

---
+++ components/component-level/src/test/java/io/github/darthakiranihil/konna/level/map/KReachabilityLayerPositiveTests.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.level.map;
+
+import io.github.darthakiranihil.konna.core.struct.KVector2i;
+import io.github.darthakiranihil.konna.level.KTileInfo;
+import io.github.darthakiranihil.konna.test.KStandardTestClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class KReachabilityLayerPositiveTests extends KStandardTestClass {
+
+    private final KReachabilityAreaLayer reachabilityAreaLayer;
+
+    public KReachabilityLayerPositiveTests() {
+
+        KTileLayer layer = new KTileLayer(3, 3);
+        KTileInfo tileInfo1 = new KTileInfo(1, true, 16, Map.of());
+        KTileInfo tileInfo2 = new KTileInfo(2, false, 16, Map.of());
+
+        layer
+            .placeTile(new KVector2i(0, 0), tileInfo1)
+            .placeTile(0, 1, tileInfo1)
+            .placeTile(0, 2, tileInfo1)
+            .placeTile(1, 0, tileInfo2)
+            .placeTile(1, 1, tileInfo2)
+            .placeTile(1, 2, tileInfo2)
+            .placeTile(2, 0, tileInfo1)
+            .placeTile(2, 1, tileInfo1)
+            .placeTile(2, 2, tileInfo1);
+
+        this.reachabilityAreaLayer = new KReachabilityAreaLayer(layer);
+
+        Assertions.assertEquals(layer.getSize(), this.reachabilityAreaLayer.getSize());
+    }
+
+    @Test
+    public void testReachable() {
+
+        Assertions.assertTrue(
+            this.reachabilityAreaLayer.isReachable(
+                new KVector2i(0, 0),
+                new KVector2i(0, 2)
+            )
+        );
+        Assertions.assertTrue(
+            this.reachabilityAreaLayer.isReachable(
+                new KVector2i(2, 0),
+                new KVector2i(2, 2)
+            )
+        );
+        Assertions.assertTrue(
+            this.reachabilityAreaLayer.isReachable(
+                0, 0, 0, 2
+            )
+        );
+        Assertions.assertTrue(
+            this.reachabilityAreaLayer.isReachable(
+                2, 0, 2, 2
+            )
+        );
+
+    }
+
+    @Test
+    public void testUnreachableBecauseOfDifferentAreas() {
+
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(
+                new KVector2i(0, 0),
+                new KVector2i(2, 2)
+            )
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(
+                new KVector2i(2, 0),
+                new KVector2i(0, 2)
+            )
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(
+                0, 0, 2, 2
+            )
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(
+                2, 0, 0, 2
+            )
+        );
+
+    }
+
+    @Test
+    public void testUnreachableBecauseOfOutOfBounds() {
+
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(-1, 0), new KVector2i(0, 1))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(3, 0), new KVector2i(0, 1))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, -1), new KVector2i(0, 1))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 3), new KVector2i(0, 1))
+        );
+
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 1), new KVector2i(-1, 0))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 1), new KVector2i(3, 0))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 1), new KVector2i(0, -1))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 1), new KVector2i(0, 3))
+        );
+
+    }
+
+    @Test
+    public void testUnreachableBecauseOfImpassableTile() {
+
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(new KVector2i(0, 0), new KVector2i(1, 1))
+        );
+        Assertions.assertFalse(
+            this.reachabilityAreaLayer.isReachable(0, 0, 1, 1)
+        );
+
+    }
+}
