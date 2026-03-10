6c66a245ca1b1df27fe936859476ece0a880b948
DarthAkiraNihil <egorzverev666@gmail.com>
2026-03-10 15:37:00 +0300

    [#178] ref: move graph implementations to core

--- core/src/test/java/io/github/darthakiranihil/konna/core/struct/graph/KHashMapColoredIntWeightMapPositiveTests.java
+++ core/src/test/java/io/github/darthakiranihil/konna/core/struct/graph/KHashMapColoredIntWeightMapPositiveTests.java
@@ -16,7 +16,6 @@
 
 package io.github.darthakiranihil.konna.core.struct.graph;
 
-import io.github.darthakiranihil.konna.core.struct.graph.KColoredIntWeightedGraph;
 import io.github.darthakiranihil.konna.test.KStandardTestClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
--- core/src/test/java/io/github/darthakiranihil/konna/core/struct/graph/KHashMapIntWeightedGraphPositiveTests.java
+++ core/src/test/java/io/github/darthakiranihil/konna/core/struct/graph/KHashMapIntWeightedGraphPositiveTests.java
@@ -16,7 +16,6 @@
 
 package io.github.darthakiranihil.konna.core.struct.graph;
 
-import io.github.darthakiranihil.konna.core.struct.graph.KIntWeightedGraph;
 import io.github.darthakiranihil.konna.test.KStandardTestClass;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
