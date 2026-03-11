2b4285cedbada20d6174c2ae5e7e1c9f63b4c7d7
DarthAkiraNihil <egorzverev666@gmail.com>
2026-03-11 09:06:37 +0300

    [#184] docs: add docs

--- components/component-level/src/main/java/io/github/darthakiranihil/konna/level/map/KHeightLayer.java
+++ components/component-level/src/main/java/io/github/darthakiranihil/konna/level/map/KHeightLayer.java
@@ -19,20 +19,48 @@ package io.github.darthakiranihil.konna.level.map;
 import io.github.darthakiranihil.konna.core.struct.KSize;
 import io.github.darthakiranihil.konna.core.struct.KVector2i;
 
+/**
+ * Map layer containing information about heights in the assigned sector.
+ *
+ * @since 0.5.0
+ * @author Darth Akira Nihil
+ */
 public final class KHeightLayer {
 
     private final int[][] heights;
     private final KSize size;
 
+    /**
+     * Constructs a layer with zero height for all places.
+     * @param size Size of the layer
+     */
     public KHeightLayer(KSize size) {
         this.heights = new int[size.height()][size.width()];
         this.size = size;
     }
 
+    /**
+     * @return Size of this layer
+     */
+    public KSize getSize() {
+        return this.size;
+    }
+
+    /**
+     * @param position Position to get height of
+     * @return Height of specific position or {@link Integer#MAX_VALUE} if the position is not
+     *         valid for this layer
+     */
     public int getOnPosition(final KVector2i position) {
         return this.getOnPosition(position.x(), position.y());
     }
 
+    /**
+     * @param x X coordinate of position to get height of
+     * @param y Y coordinate of position to get height of
+     * @return Height of specific position or {@link Integer#MAX_VALUE} if the position is not
+     *         valid for this layer
+     */
     public int getOnPosition(int x, int y) {
         if (x >= this.size.width() || x < 0 || y >= this.size.height() || y < 0) {
             return Integer.MAX_VALUE;
@@ -41,10 +69,25 @@ public final class KHeightLayer {
         return this.heights[y][x];
     }
 
+    /**
+     * Sets height for specific position. It does not have any effect if the position is not valid
+     * for this layer.
+     * @param position Position to set height for
+     * @param height New height value
+     * @return This layer (for method chaining)
+     */
     public KHeightLayer setHeight(final KVector2i position, int height) {
         return this.setHeight(position.x(), position.y(), height);
     }
 
+    /**
+     * Sets height for specific position. It does not have any effect if the position is not valid
+     * for this layer.
+     * @param x X coordinate of position to set height for
+     * @param y Y coordinate of position to set height for
+     * @param height New height value
+     * @return This layer (for method chaining)
+     */
     public KHeightLayer setHeight(int x, int y, int height) {
         if (x >= this.size.width() || x < 0 || y >= this.size.height() || y < 0) {
             return this;
