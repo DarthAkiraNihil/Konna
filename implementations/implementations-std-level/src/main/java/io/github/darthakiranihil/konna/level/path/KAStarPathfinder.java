e4e06c3000c4491d01622c9ce635daf1097e43a6
DarthAkiraNihil <egorzverev666@gmail.com>
2026-03-11 15:52:34 +0300

    [#183] feat: add implementations module for level

---
+++ implementations/implementations-std-level/src/main/java/io/github/darthakiranihil/konna/level/path/KAStarPathfinder.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.darthakiranihil.konna.level.path;
+
+import io.github.darthakiranihil.konna.level.map.KLocation;
+
+public class KAStarPathfinder implements KPathfinder {
+
+    @Override
+    public KPath findPath(
+        final KLocation location,
+        final String srcSector,
+        int srcX,
+        int srcY,
+        final String dstSector,
+        int dstX,
+        int dstY
+    ) {
+        return null;
+    }
+
+}
---
+++ implementations/implementations-std-level/src/main/java/io/github/darthakiranihil/konna/level/path/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides standard implementations of path-related abstractions.
+ */
+@NullMarked
+package io.github.darthakiranihil.konna.level.path;
+
+import org.jspecify.annotations.NullMarked;
