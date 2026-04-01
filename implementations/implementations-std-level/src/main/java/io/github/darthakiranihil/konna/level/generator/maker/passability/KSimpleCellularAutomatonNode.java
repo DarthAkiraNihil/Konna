58bbd1820d34373a14390d89fe76be29715d51c0
DarthAkiraNihil <egorzverev666@gmail.com>
2026-04-01 12:17:12 +0300

    [#158] feat: add noise layer

--- implementations/implementations-std-level/src/main/java/io/github/darthakiranihil/konna/level/generator/maker/passability/KSimpleCellularAutomatonNode.java
+++ implementations/implementations-std-level/src/main/java/io/github/darthakiranihil/konna/level/generator/maker/passability/KSimpleCellularAutomatonNode.java
@@ -16,5 +16,83 @@
 
 package io.github.darthakiranihil.konna.level.generator.maker.passability;
 
-public final class KSimpleCellularAutomatonNode {
+import io.github.darthakiranihil.konna.core.data.KUniversalMap;
+import io.github.darthakiranihil.konna.core.except.KInvalidArgumentException;
+import io.github.darthakiranihil.konna.core.struct.KPair;
+import io.github.darthakiranihil.konna.core.struct.KSize;
+import io.github.darthakiranihil.konna.level.generator.KGeneratorNode;
+import io.github.darthakiranihil.konna.level.generator.KGeneratorNodeInputParam;
+import io.github.darthakiranihil.konna.level.generator.KGeneratorNodeOutputParam;
+import io.github.darthakiranihil.konna.level.layer.KPassabilityLayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public final class KSimpleCellularAutomatonNode implements KGeneratorNode {
+
+    @Override
+    @KGeneratorNodeInputParam(name = "size", type = KSize.class)
+    @KGeneratorNodeInputParam(name = "rule", type = String.class)
+    @KGeneratorNodeOutputParam(name = "layer", type = KPassabilityLayer.class)
+    // B2/S123
+    public KUniversalMap process(final KUniversalMap params, final Random rnd) {
+
+        KSize size = params.get("size", KSize.class);
+        String rule = params.get("rule", String.class);
+
+        var parsedRule = this.parseRule(rule);
+        KPassabilityLayer layer = new KPassabilityLayer(size);
+
+        return null;
+    }
+
+    private KPair<List<Integer>, List<Integer>> parseRule(final String rule) {
+        var parts = rule.split("/");
+        if (parts.length != 2) {
+            throw new KInvalidArgumentException(
+                String.format(
+                    "Cellular automaton rule is not valid: %s",
+                    rule
+                )
+            );
+        }
+
+        String bornRule = parts[0];
+        if (bornRule.charAt(0) != 'B') {
+            throw new KInvalidArgumentException(
+                String.format(
+                    "The first char of \"born\" must be B, but got %s",
+                    bornRule.charAt(0)
+                )
+            );
+        }
+
+        String stayRule = parts[1];
+        if (stayRule.charAt(0) != 'B') {
+            throw new KInvalidArgumentException(
+                String.format(
+                    "The first char of \"stay\" must be S, but got %s",
+                    stayRule.charAt(0)
+                )
+            );
+        }
+
+        ArrayList<Integer> born = new ArrayList<>(8);
+        for (int i = 1; i < bornRule.length(); i++) {
+            born.add(Character.getNumericValue(bornRule.charAt(i)));
+        }
+
+        ArrayList<Integer> stay = new ArrayList<>(8);
+        for (int i = 1; i < stayRule.length(); i++) {
+            stay.add(Character.getNumericValue(stayRule.charAt(i)));
+        }
+
+        born.trimToSize();
+        stay.trimToSize();
+
+        return new KPair<>(
+            born, stay
+        );
+    }
 }
---
+++ implementations/implementations-std-level/src/main/java/io/github/darthakiranihil/konna/level/generator/mutator/entity/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package io.github.darthakiranihil.konna.level.generator.mutator.entity;
+
+import org.jspecify.annotations.NullMarked;
