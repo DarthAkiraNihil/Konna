8cb9ff62e026a4a1f5ad843ec5d17acc913daa58
DarthAkiraNihil <egorzverev666@gmail.com>
2026-04-09 08:00:06 +0300

    [#243] del: remove KSingleton.weak

--- annotations/annotations-core/src/main/java/io/github/darthakiranihil/konna/core/object/KSingleton.java
+++ annotations/annotations-core/src/main/java/io/github/darthakiranihil/konna/core/object/KSingleton.java
@@ -23,7 +23,7 @@ import java.lang.annotation.Target;
 
 /**
  * Marks class as a singleton so there should be created only one
- * instance of it. A singleton object cannot be both immortal and weak.
+ * instance of it.
  *
  * @since 0.2.0
  * @author Darth Akira Nihil
@@ -31,12 +31,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface KSingleton {
-    /**
-     * Flag that specifies if a singleton should be created and stored
-     * using a weak reference (if {@code true}) or not. Defaults to {@code false}
-     * @return Flag of weak reference usage
-     */
-    boolean weak() default false;
 
     /**
      * Flag that indicates if singleton is immortal, so it cannot be "deleted"
