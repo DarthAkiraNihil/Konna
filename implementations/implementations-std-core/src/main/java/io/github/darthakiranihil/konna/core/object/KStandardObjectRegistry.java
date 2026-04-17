0861caac4d81bd4607533f3846d0ef9137492d3a
DarthAkiraNihil <egorzverev666@gmail.com>
2026-04-17 20:16:05 +0300

    [#233] ref: rework KStandardObjectRegistry to use PhantomReferences

--- implementations/implementations-std-core/src/main/java/io/github/darthakiranihil/konna/core/object/KStandardObjectRegistry.java
+++ implementations/implementations-std-core/src/main/java/io/github/darthakiranihil/konna/core/object/KStandardObjectRegistry.java
@@ -23,12 +23,11 @@ import io.github.darthakiranihil.konna.core.di.KInject;
 import io.github.darthakiranihil.konna.core.object.except.KDeletedObjectException;
 import org.jspecify.annotations.Nullable;
 
+import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -40,7 +39,6 @@ import java.util.stream.Collectors;
  * @since 0.2.0
  * @author Darth Akira Nihil
  */
-// todo: rework on phantom references, but when new managed object model will appear
 public final class KStandardObjectRegistry extends KObject implements KObjectRegistry {
 
     private static final int INITIAL_CAPACITY = 64;
@@ -120,7 +118,10 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
 
         private final WeakReference<?> ref;
 
-        CommonObjectRecord(final Object object, final WeakReference<?> ref) {
+        CommonObjectRecord(
+            final Object object,
+            final WeakReference<?> ref
+        ) {
             super(object);
             this.ref = ref;
         }
@@ -197,6 +198,7 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
     );
 
     private final Map<UUID, KObjectRegistryRecord> records;
+    private final Queue<PhantomReference<?>> phantoms;
     private final ReferenceQueue<Object> referenceQueue;
 
     @KInject
@@ -212,17 +214,19 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
         );
 
         this.records = new ConcurrentHashMap<>(INITIAL_CAPACITY);
+        this.phantoms = new ArrayBlockingQueue<>(INITIAL_CAPACITY);
         this.referenceQueue = new ReferenceQueue<>();
 
-        frameTaskScheduler.scheduleTask(REMOVE_DEAD_RECORDS_TASK, this::removeDeadObjects);
+        frameTaskScheduler.scheduleTask(REMOVE_DEAD_RECORDS_TASK, this::postMortem);
     }
 
     @Override
     public KObjectRegistryRecord pushObject(final Object object) {
 
-        WeakReference<?> ref = new WeakReference<>(object, this.referenceQueue);
+        WeakReference<?> ref = new WeakReference<>(object);
         KObjectRegistryRecord objectRecord = new CommonObjectRecord(object, ref);
         this.records.put(objectRecord.objectId(), objectRecord);
+        this.phantoms.add(new PhantomReference<>(object, this.referenceQueue));
         return objectRecord;
 
     }
@@ -232,6 +236,7 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
 
         KObjectRegistryRecord objectRecord = new ImmortalObjectRecord(object);
         this.records.put(objectRecord.objectId(), objectRecord);
+        this.phantoms.add(new PhantomReference<>(object, this.referenceQueue));
         return objectRecord;
 
     }
@@ -243,8 +248,8 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
 
     @Override
     public void removeObject(final UUID objectId) {
-        // todo: phantomize but on new managed object model
         this.records.remove(objectId);
+
     }
 
     @Override
@@ -270,15 +275,29 @@ public final class KStandardObjectRegistry extends KObject implements KObjectReg
         return Set.copyOf(this.records.values());
     }
 
-    private void removeDeadObjects() {
+    private void postMortem() {
+        Set<UUID> eliminated = this.records
+            .entrySet()
+            .stream()
+            .filter(x -> !x.getValue().isPresent())
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toUnmodifiableSet());
+
         this.records
             .entrySet()
-            .removeIf(x -> !x.getValue().isPresent());
+            .removeIf(x -> eliminated.contains((x.getKey())));
 
         Object deleted = this.referenceQueue.poll();
         while (deleted != null) {
-            // todo: add check if object is managed, then delete
+            Class<?> clazz = deleted.getClass();
+            if (KDeletable.class.isAssignableFrom(clazz)) {
+                ((KDeletable) deleted).delete();
+            }
             deleted = this.referenceQueue.poll();
+        }
+
+        while (!this.phantoms.isEmpty()) {
+            this.phantoms.poll(); // dummy code
         }
     }
 
