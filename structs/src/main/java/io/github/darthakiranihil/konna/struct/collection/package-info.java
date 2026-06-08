f62d698545229d04b62098001048209847c84cc7
DarthAkiraNihil <egorzverev666@gmail.com>
2026-06-08 11:04:16 +0300

    [#301] feat: add iterators

---
+++ structs/src/main/java/io/github/darthakiranihil/konna/struct/collection/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides collections, data containers and other related classes.
+ *
+ * @since 0.7.0
+ * @author Darth Akira Nihil
+ */
+@NullMarked
+package io.github.darthakiranihil.konna.struct.collection;
+
+import org.jspecify.annotations.NullMarked;
