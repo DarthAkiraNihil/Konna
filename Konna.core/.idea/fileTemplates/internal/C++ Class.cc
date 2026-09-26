96f92b827faa02905a52a5660adce27196d9318c
Egor Zverev <62760806+DarthAkiraNihil@users.noreply.github.com>
2026-09-26 08:04:04 +0300

    [#5] dev: configure CMake presets

--- Konna.core/.idea/fileTemplates/internal/C++ Class.cc
+++ Konna.core/.idea/fileTemplates/internal/C++ Class.cc
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#parse("C File Header.h")
 #[[#include]]# "${HEADER_FILENAME}"
 
 ${NAMESPACES_OPEN_CPP}
--- Konna.core/.idea/fileTemplates/internal/C++ Module File.ixx
+++ Konna.core/.idea/fileTemplates/internal/C++ Module File.ixx
@@ -14,5 +14,4 @@
  * limitations under the License.
  */
 
-#parse("C File Header.h")
 export module ${NAME};
